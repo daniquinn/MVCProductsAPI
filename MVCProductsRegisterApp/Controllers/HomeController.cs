ca248093d00d573745c162ad9ec5090f8e0623ac
daniquinn <daniel.pereiraaraujo@outlook.com>
2021-08-27 17:22:33 -0300

    Criando o Helper

--- MVCProductsRegisterApp/Controllers/HomeController.cs
+++ MVCProductsRegisterApp/Controllers/HomeController.cs
@@ -2,26 +2,15 @@
 using Microsoft.Extensions.Logging;
 using MVCProductsRegisterApp.Helper;
 using MVCProductsRegisterApp.Models;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MVCProductsRegisterApp.Controllers
 {
     public class HomeController : Controller
     {
-        ProductAPI _api = new ProductAPI();
-
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Privacy()
         {
             return View();
@@ -32,5 +21,12 @@ namespace MVCProductsRegisterApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
     }
 }
