3ca984585e2918a8536947d42dfadf7f14b23f1d
daniquinn <daniel.pereiraaraujo@outlook.com>
2021-08-27 17:03:04 -0300

    Iniciando projeto de aplicação web para consumir a webapi

---
+++ MVCProductsRegisterApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCProductsRegisterApp.Helper;
+using MVCProductsRegisterApp.Models;
+using System.Diagnostics;
+
+namespace MVCProductsRegisterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductAPI _api = new ProductAPI();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCProductsRegisterApp/Helper/ProductAPI.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+namespace MVCProductsRegisterApp.Helper
+{
+    public class ProductAPI
+    {
+        public HttpClient Initial()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new System.Uri("https://localhost:44396/");
+            return client;
+        }
+    }
+}
