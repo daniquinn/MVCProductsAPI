a132eae53535e06ee23db15e0e3fd0a5835734dc
daniquinn <daniel.pereiraaraujo@outlook.com>
2021-08-27 17:34:25 -0300

    Configurando o HomeController

--- MVCProductsRegisterApp/Controllers/HomeController.cs
+++ MVCProductsRegisterApp/Controllers/HomeController.cs
@@ -6,11 +6,27 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace MVCProductsRegisterApp.Controllers
 {
     public class HomeController : Controller
     {
+        ProductAPI _api = new ProductAPI();
+
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = new List<Product>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/products");
+            if (res.IsSuccessStatusCode)
+            {
+                var results = res.Content.ReadAsStringAsync().Result;
+                products = JsonConvert.DeserializeObject<List<Product>>(results);
+            }
+            return View(products);
+        }
+
         public IActionResult Privacy()
         {
             return View();
